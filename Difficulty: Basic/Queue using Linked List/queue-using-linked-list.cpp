09917229e45e3c89bbe1c2b31f7dec8677c2b0ae
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-07-25 12:57:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Queue using Linked List/queue-using-linked-list.cpp
+++ Difficulty: Basic/Queue using Linked List/queue-using-linked-list.cpp
@@ -3,52 +3,48 @@ class Node {
     int data;
     Node* next;
 
-    Node(int new_data) {
-        data = new_data;
+    Node(int val) {
+        data = val;
         next = nullptr;
     }
 };
 
 class myQueue {
-    Node* head;
+    Node * right;
     Node* left;
     int si;
   public:
     myQueue() {
         // Initialize your data members
-        head=NULL;
-        left=NULL;
+        left=nullptr;
+        right=nullptr;
         si=0;
     }
 
     bool isEmpty() {
         // check if the queue is empty
-        if(left==NULL){
-            return true;
-        }
-        return false;
+        return si==0;
     }
 
     void enqueue(int x) {
         // Adds an element x at the rear of the queue
-        if(left==nullptr){
+        if(left==NULL){
             Node* temp=new Node(x);
             left=temp;
-            head=temp;
+            right=temp;
             si++;
             return;
         }
-        Node * temp=new Node(x);
-        head->next=temp;
-        head=head->next;
+        Node* temp=new Node(x);
+        right->next=temp;
+        right=right->next;
         si++;
-        return;
     }
 
     void dequeue() {
         // Removes the front element of the queue
-        if(left==NULL){
-            return ;
+        if(isEmpty()){
+            return;
         }
         left=left->next;
         si--;
@@ -57,11 +53,10 @@ class myQueue {
     int getFront() {
         // Returns the front element of the queue
         // If queue is empty, return -1
-        if(left==NULL){
+        if(isEmpty()){
             return -1;
         }
-        int n=left->data;
-        return n;
+        return left->data;
     }
 
     int size() {
