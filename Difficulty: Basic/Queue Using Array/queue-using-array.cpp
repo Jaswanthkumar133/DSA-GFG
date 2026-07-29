af31058f20d8058299e1fd0f5d825918639c849e
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-07-29 22:08:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Queue Using Array/queue-using-array.cpp
+++ Difficulty: Basic/Queue Using Array/queue-using-array.cpp
@@ -2,16 +2,17 @@ class myQueue {
     int *arr;
     int left;
     int right;
-    int size;
+    int si;
     int count;
   public:
     myQueue(int n) {
         // Define Data Structures
         arr=new int[n];
-        size=n;
+        si=n;
         left=0;
         right=-1;
         count=0;
+        
     }
 
     bool isEmpty() {
@@ -21,13 +22,13 @@ class myQueue {
 
     bool isFull() {
         // check if the queue is full
-        return count==size;
+        return count==si;
     }
 
     void enqueue(int x) {
         // Adds an element x at the rear of the queue.
-        if(!isFull()){
-            right=(right+1)%size;
+        if(count<si){
+            right=(right+1)%si;
             arr[right]=x;
             count++;
         }
@@ -35,14 +36,14 @@ class myQueue {
 
     void dequeue() {
         // Removes the front element of the queue.
-        if(!isEmpty()){
-            left=(left+1)%size;
+        if(count>0){
+            left=(left+1)%si;
             count--;
         }
     }
 
     int getFront() {
-        // Returns the front element of the queue.
+        // Returns the front element of the queue
         if(!isEmpty()){
             return arr[left];
         }
