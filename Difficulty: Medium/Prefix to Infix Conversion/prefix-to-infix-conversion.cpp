66be1468dce57121a9bd954b34104ed76d73d86d
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-07-30 00:32:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.cpp
+++ Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.cpp
@@ -5,14 +5,13 @@ class Solution {
         stack<string>st;
         for(int i=s.size()-1;i>=0;i--){
             if(isalnum(s[i])){
-                string ss=string(1,s[i]);
-                st.push(ss);
+                st.push(string(1,s[i]));
             }else{
                 string s1=st.top();
                 st.pop();
                 string s2=st.top();
                 st.pop();
-                st.push('('+s1+s[i]+s2+')');
+                st.push("("+s1+string(1,s[i])+s2+")");
             }
         }
         return st.top();
