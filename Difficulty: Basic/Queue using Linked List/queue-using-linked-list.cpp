16ac31155b2fd947f29f565c319718b5d53fefa1
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-07-31 23:34:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Queue using Linked List/queue-using-linked-list.cpp
+++ Difficulty: Basic/Queue using Linked List/queue-using-linked-list.cpp
@@ -1,66 +1,66 @@
 class Node {
-  public:
-    int data;
-    Node* next;
-
-    Node(int val) {
-        data = val;
-        next = nullptr;
-    }
+	public:
+	int data;
+	Node* next;
+	
+	Node(int val) {
+		data = val;
+		next = nullptr;
+	}
 };
 
 class myQueue {
-    Node*left;
-    Node* right;
-    int si;
-  public:
-    myQueue() {
-        // Initialize your data members
-        left=NULL;
-        right=NULL;
-        si=0;
-    }
-
-    bool isEmpty() {
-        // check if the queue is 
-        return si==0;
-        
-    }
-
-    void enqueue(int x) {
-        // Adds an element x at the rear of the queue
-        if(left==NULL){
-            Node* temp=new Node(x);
-            left=temp;
-            right=temp;
-            si++;
-            return;
-        }
-        Node*temp=new Node(x);
-        right->next=temp;
-        right=right->next;
-        si++;
-    }
-
-    void dequeue() {
-        // Removes the front element of the 
-        if(!isEmpty()){
-            left=left->next;
-            si--;
-        }
-    }
-
-    int getFront() {
-        // Returns the front element of the queue
-        // If queue is empty, return -1
-        if(!isEmpty()){
-            return left->data;
-        }
-        return -1;
-    }
-
-    int size() {
-        // Returns the current size of the queue
-        return si;
-    }
+	Node*left;
+	Node* right;
+	int si;
+	
+	public:
+	myQueue() {
+		// Initialize your data members
+		left=NULL;
+		right=NULL;
+		si=0;
+	}
+	
+	bool isEmpty() {
+		// check if the queue is empty
+		return left==NULL;
+	}
+	
+	void enqueue(int x) {
+		// Adds an element x at the rear of the queue
+		if(left==NULL){
+		    Node* temp=new Node(x);
+		    left=temp;
+		    right=temp;
+		    si++;
+		    return;
+		}
+		Node* temp=new Node(x);
+		right->next=temp;
+		right=right->next;
+		si++;
+	}
+	
+	void dequeue() {
+		// Removes the front element of the queue
+		if(left){
+		    left=left->next;
+		    si--;
+		}
+	}
+	
+	int getFront() {
+		// Returns the front element of the queue
+		// If queue is empty, return -1
+		if(si>0){
+		    return left->data;
+		}
+		return -1;
+	}
+	
+	int size() {
+		// Returns the current size of the queue.
+		return si;
+	}
 };
