577345aac861237249974e32237d4e9ade3ea360
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-08-01 02:21:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
@@ -1,45 +1,44 @@
 class Solution {
-	public:
-	int prec(char ch) {
-		if (ch == '^') {
-			return 3;
-		}
-		else if (ch == '*' || ch == '/') {
-			return 2;
-		} else if (ch == '+' || ch == '-') {
-			return 1;
-		}
-		return 0;
-	}
-	string infixToPostfix(string& s) {
-		// code here
-		string ans = "";
-		stack<int>st;
-		for (char ch:s) {
-			if (isalnum(ch)) {
-				ans += ch;
-			} else if (ch == '(') {
-				st.push(ch);
-			} else if (ch == ')') {
-				while (!st.empty() && st.top() != '(') {
-					ans += st.top();
-					st.pop();
-				}
-				if (!st.empty()) {
-					st.pop();
-				}
-			} else {
-				while (!st.empty() && (prec(ch)<prec(st.top()) || prec(ch) == prec(st.top())) && ch != '^') {
-					ans += st.top();
-					st.pop();
-				}
-				st.push(ch);
-			}
-		}
-		while (!st.empty()) {
-			ans += st.top();
-			st.pop();
-		}
-		return ans;
-	}
+  public:
+  int prec(char ch){
+      if(ch=='^'){
+          return 3;
+      }else if(ch=='*' || ch=='/'){
+          return 2;
+      }else if(ch=='+' || ch=='-'){
+          return 1;
+      }
+      return 0;
+  }
+    string infixToPostfix(string& s) {
+        // code here
+        stack<char>st;
+        string ans;
+        for(char ch:s){
+            if(isalnum(ch)){
+                ans+=ch;
+            }else if(ch=='('){
+                st.push(ch);
+            }else if( ch==')'){
+                while(!st.empty() && st.top()!='('){
+                    ans+=st.top();
+                    st.pop();
+                }
+                if(!st.empty()){
+                    st.pop();
+                }
+            }else{
+                while(!st.empty() && (prec(ch)<prec(st.top()) || prec(ch)==prec(st.top()))&& ch!='^'){
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.push(ch);
+            }
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans;
+    }
 };
