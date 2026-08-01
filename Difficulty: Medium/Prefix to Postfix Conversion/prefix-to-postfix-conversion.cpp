40cdb82fda83c319b7107db6bc2cbbe5db239bc4
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-08-01 12:16:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Prefix to Postfix Conversion/prefix-to-postfix-conversion.cpp
+++ Difficulty: Medium/Prefix to Postfix Conversion/prefix-to-postfix-conversion.cpp
@@ -3,16 +3,16 @@ class Solution {
     string preToPost(string &s) {
         // code here
         stack<string>st;
-        string ans="";
-        for(int i=s.size()-1;i>=0;i--){
-            if(isalnum(s[i])){
-                st.push(string(1,s[i]));
+        reverse(s.begin(),s.end());
+        for(char c:s){
+            if(isalnum(c)){
+                st.push(string(1,c));
             }else{
                 string s1=st.top();
                 st.pop();
                 string s2=st.top();
                 st.pop();
-                st.push(s1+s2+s[i]);
+                st.push(s1+s2+string(1,c));
             }
         }
         return st.top();
