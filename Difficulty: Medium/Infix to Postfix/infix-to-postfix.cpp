769e834466fe4b3570e76f99fe851afb5eb09221
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-08-07 03:17:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
@@ -1,46 +1,44 @@
 class Solution {
-	public:
-	int prec(char ch) {
-		if (ch == '^') {
-			return 3;
-		} else if (ch == '*' || ch == '/') {
-			return 2;
-		} else if (ch == '+' || ch == '-') {
-			return 1;
-		}
-		return 0;
-	}
-	string infixToPostfix(string& s) {
-		// code
-		stack<int>st;
-		string ans = "";
-		for (int i = 0; i<s.size(); i++) {
-			if (isalnum(s[i])) {
-				ans += s[i];
-			} else if (s[i] == '(') {
-				st.push(s[i]);
-			} else if (s[i] == ')') {
-				while (!st.empty() && st.top() != '(') {
-					ans += st.top();
-					st.pop();
-				}
-				if (!st.empty()) {
-					st.pop();
-				}
-			} else {
-				while (!st.empty() && (prec(s[i])<prec(st.top()) || (prec(s[i]) == prec(st.top()) && s[i] != '^'))) {
-					ans += st.top();
-					st.pop();
-					
-				}
-				st.push(s[i]);
-			}
-		}
-		while (!st.empty()) {
-			ans += st.top();
-			st.pop();
-		}
-		return ans;
-		
-	}
+  public:
+    int prec(char ch){
+        if(ch=='^'){
+            return 3;
+        }else if(ch=='*' || ch=='/'){
+            return 2;
+        }else if(ch=='+'||ch=='-'){
+            return 1;
+        }
+        return 0;
+    }
+    string infixToPostfix(string& s) {
+        // code here
+        string ans="";
+        stack<char>st;
+        for(char c:s){
+            if(isalnum(c)){
+                ans+=c;
+            }else if(c=='('){
+                st.push(c);
+            }else if(c==')'){
+                while(!st.empty() && st.top()!='('){
+                    ans+=st.top();
+                    st.pop();
+                }
+                if(!st.empty()){
+                    st.pop();
+                }
+            }else{
+                while(!st.empty() &&(prec(c)<prec(st.top()) ||(prec(c)==prec(st.top())&& c!='^'))){
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.push(c);
+            }
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans;
+    }
 };
